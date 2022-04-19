50b8a210d0ac755d041542539e95656938218227
Ronti <nasowasaberauch@posteo.de>
2022-04-19 10:35:52 +0200

    remove visual studio files, rename source folder

--- source/FlatApproach.h
+++ source/FlatApproach.h
@@ -38,6 +38,12 @@ public:
         doWorkArrayImpl(0, callable, indices);
     }
 
+    template<class Calleable>
+    void doWorkPack(const Calleable& callable) const noexcept
+    {
+        doWorkPackImpl(0, callable);
+    }
+
 protected:
     int idxImpl(const Ints& params, const int& dim = 0) const noexcept
     {
@@ -62,12 +68,41 @@ protected:
     }
 
     template<class Calleable>
-    void doWorkImpl(const int& dim, const Calleable& callable, Ints& indices) const noexcept
+    void doWorkArrayImpl(const int& dim, const Calleable& callable, Ints& indices) const noexcept
     {
-        if(dim == Size - 1)
-            callable(indices);
-        else
-            doWorkArrayImpl(dim + 1, callable, indices);
+        const auto final = dim == Size - 1;
+        const auto& s = m_Sizes[dim];
+
+        for(int i = 0; i < s; i++)
+        {
+            indices[dim] = i;
+
+            if(final)
+                callable(indices);
+            else
+                doWorkArrayImpl(dim + 1, callable, indices);
+        }
+    }
+
+    template<class... Dims>
+    struct ParamPack
+    {
+        Dims... m_Pack;
+    };
+
+    template<class Calleable>
+    void doWorkPackImpl(const int& dim, const Calleable& callable, const ParamPack<>& paramPack = ParamPack<>()) const noexcept
+    {
+        const auto final = dim == Size - 1;
+        const auto& s = m_Sizes[dim];
+
+        for(int i = 0; i < s; i++)
+        {
+            if(final)
+                return; //callable(indices);
+            else
+                doWorkPackImpl(dim + 1, callable, ParamPack(paramPack, i));
+        }
     }
 
 private:
